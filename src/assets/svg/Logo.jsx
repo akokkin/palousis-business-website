5a297a617f2a779908a8142f108f11a7fddcc997
Alexandros <akokkinn@gmail.com>
2025-05-05 20:28:06 +0300

    fix: refactor SvgComponent() to return logo based on variant

--- src/assets/svg/Logo.jsx
+++ src/assets/svg/Logo.jsx
@@ -1,15 +1,25 @@
 import * as React from "react";
+import LogoImage from "../img/logo-transparent-black.png";
+import WhiteLogoImage from "../img/logo-transparent-white.png";
 
-function SvgComponent(props) {
+function SvgComponent({ variant = "default", ...props }) {
+  const logoSrc = variant === "white" ? WhiteLogoImage : LogoImage;
+  
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={27} height={40} viewBox="0 0 27 40" {...props}>
-      <g data-name="Group 101" transform="translate(-375 -21)">
-        <rect data-name="Rectangle 16" width={13} height={8} rx={4} transform="translate(375 53)" fill="#f40051" />
-        <rect data-name="Rectangle 11" width={20} height={8} rx={4} transform="translate(382 21)" fill="#f2b300" />
-        <rect data-name="Rectangle 15" width={20} height={8} rx={4} transform="translate(382 37)" fill="#7620ff" />
-        <rect data-name="Rectangle 12" width={8} height={8} rx={4} transform="translate(375 29)" fill="#4cd5c5" />
-      </g>
-    </svg>
+    <img
+      src={logoSrc}
+      alt="Palousis Logo"
+      style={{
+        maxHeight: '60px',
+        maxWidth: '100%',
+        width: 'auto',
+        height: 'auto',
+        objectFit: 'contain',
+        paddingTop: '5px',
+        paddingBottom: '5px',
+      }}
+      {...props}
+    />
   );
 }
 
